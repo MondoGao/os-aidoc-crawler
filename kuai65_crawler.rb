176a2d58f4ee1922d4d223122b5060212929ad56
Mondo <mondogao@gmail.com>
2017-03-12 23:35:57 +0800

    feat: 基本完成模版 pdf 爬虫

--- kuai65_crawler.rb
+++ kuai65_crawler.rb
@@ -3,9 +3,12 @@ require 'open-uri'
 require 'thread'
 require 'uri'
 require 'fileutils'
+require 'json'
 require 'bundler/setup'
 Bundler.require
 
+data_file = File.open('./benke_template_data.json', 'r+')
+data = JSON.parse(data_file.read)
 
 agent = Mechanize.new
 page = agent.get('http://www.kuai65.com/users/sign_in')
@@ -13,7 +16,19 @@ login_form = page.forms[0]
 login_form['user[phone]'] = '18672310260'
 login_form['user[password]'] = '#&&j8jKa9YW3'
 agent.submit login_form
-agent.click agent.page.link_with(:href => '/theses').click
-agent.click agent.page.link_with(:href => "/theses/3242/covers/common")
-agent.click agent.page.at_css('.ke-icon-generate_word')
+
+data.each do |paper|
+  agent.get('http://www.kuai65.com/theses/3242/templates/edit')
+  agent.page.form['standard_id'] = paper['id']
+  agent.submit agent.page.form
+  agent.click agent.page.at_css('.ke-icon-generate_word')
+  agent.submit agent.page.form
+  download_link = agent.page.links.select {|link| link.href.match /ft\=pdf/}[0]
+  while !download_link
+    sleep 1
+    agent.get agent.page.uri.to_s
+    download_link = agent.page.links.select {|link| link.href.match /ft\=pdf/}[0]
+  end
+  agent.download(download_link.href, "templates/#{paper['name']}.pdf")
+end
 binding.pry
